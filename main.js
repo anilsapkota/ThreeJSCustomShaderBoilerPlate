3ab69f6fd5dcd69f6aba1d5a6f233572d1eb071e
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2025-05-03 11:14:18 -0500

    Update main.js
    Added the Orbit Controls as well

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import vertexShader from './shaders/vertex.glsl?raw';
 import fragmentShader from './shaders/fragment.glsl?raw';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
+
+const controls = new OrbitControls(camera,renderer.domElement);
 
 
 
@@ -36,6 +41,8 @@ function animate() {
 
   ico.rotation.x += 0.01;
   ico.rotation.y += 0.01;
+
+  controls.update();
 
   renderer.render( scene, camera );
 
